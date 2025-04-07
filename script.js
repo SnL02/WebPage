816c9065542b923fed3ab3425255bce5c96325fd
nextxjz <71954350+nextxjz@users.noreply.github.com>
2025-04-07 13:19:47 +0000

    fix: mobile view

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ document.addEventListener("DOMContentLoaded", () => {
       bar.style.width = percentage + "%";
       bar.querySelector("span").textContent = percentage + "%";
     });
+
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('nav ul');
+    
+    menuToggle.addEventListener('click', function() {
+      navMenu.classList.toggle('active');
+    });
+    
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        if (window.innerWidth <= 768) {
+          navMenu.classList.remove('active');
+        }
+      });
+    });
   });
